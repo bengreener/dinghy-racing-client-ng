d6cddb8d7aa434e7fe0347021b4b9919ecbc6364
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-06-25 22:58:35 +0100

    rename handleClick() to handleCreate()

--- src/view/CreateDinghyClass.js
+++ src/view/CreateDinghyClass.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { screen } from 'react';
 
 function CreateDinghyClass({ onCreate }) {
 
-    function handleClick(event) {
+    function handleCreate(event) {
         event.preventDefault();
         const input = document.getElementById('dinghy-class-input');
         onCreate({'name': input.value});
@@ -13,7 +12,7 @@ function CreateDinghyClass({ onCreate }) {
         <form action="" method="get">
             <label htmlFor="dinghy-class-input">Class Name</label>
             <input id="dinghy-class-input" type="text" />
-            <button id="dinghy-class-create-button" type="button" onClick={handleClick}>Create</button>
+            <button id="dinghy-class-create-button" type="button" onClick={handleCreate}>Create</button>
         </form>
     )
 }
