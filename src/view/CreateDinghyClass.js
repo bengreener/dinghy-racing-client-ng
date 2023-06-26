cd6ecbfe83a37422d296c82e9e45a715a0b45105
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-06-26 20:46:10 +0100

    update CreateDinghyClass to handle result from controller.createDinghyClass and make input controlled component

--- src/view/CreateDinghyClass.js
+++ src/view/CreateDinghyClass.js
@@ -1,17 +1,42 @@
 import React from 'react';
 
 function CreateDinghyClass({ onCreate }) {
+    const [dinghyClass, setDinghyClass] = React.useState({'name': ''});
+    const [result, setResult] = React.useState({message: ''});
+    
+    const clear = React.useCallback(() => {
+        setDinghyClass({'name': ''});
+        showMessage('');
+    }, []);
 
-    function handleCreate(event) {
+    React.useEffect(() => {
+        if (result && result.success) {
+            clear();
+        }
+        if (result && !result.success) {
+            showMessage(result.message);
+        }
+    }, [result, clear]);
+
+    async function handleCreate(event) {
         event.preventDefault();
-        const input = document.getElementById('dinghy-class-input');
-        onCreate({'name': input.value});
+        setResult(await onCreate(dinghyClass));
+    }
+
+    function handleChange({target}) {
+        setDinghyClass({...dinghyClass, [target.name]: target.value});
+    }
+
+    function showMessage(message) {
+        const output = document.getElementById('dinghy-class-message-output');
+        output.value = message;
     }
 
     return (
         <form action="" method="get">
             <label htmlFor="dinghy-class-input">Class Name</label>
-            <input id="dinghy-class-input" type="text" />
+            <input id="dinghy-class-input" name="name" type="text" onChange={handleChange} value={dinghyClass.name} />
+            <output id="dinghy-class-message-output" />
             <button id="dinghy-class-create-button" type="button" onClick={handleCreate}>Create</button>
         </form>
     )
--- src/view/CreateDinghyClass.test.js
+++ src/view/CreateDinghyClass.test.js
@@ -1,6 +1,7 @@
 import { screen, prettyDOM, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import CreateDinghyClass from './CreateDinghyClass';
+import { act } from 'react-dom/test-utils';
 
 it('renders', () => {
     render(<CreateDinghyClass />);
@@ -10,32 +11,69 @@ it('accepts the name of a dinghy class', async () => {
     const user = userEvent.setup();
     
     render(<CreateDinghyClass />);
-    const txtClassName = screen.getByLabelText('Class Name');
-    await user.type(txtClassName, 'Scorpion');
-
-    expect(txtClassName.value).toBe('Scorpion');
+    const txtClassName = await screen.findByLabelText('Class Name');
+    await act(async () => {
+        await user.type(txtClassName, 'Scorpion');
+    })
+    
+    expect(txtClassName).toHaveValue('Scorpion');
 });
 
 it('calls the function passed in to onCreate prop', async () => {
     const user = userEvent.setup();
-    const fnOnCreate = jest.fn();
+    const fnOnCreate = jest.fn(() => {return {'success': true}});
     
-    render(<CreateDinghyClass onCreate={fnOnCreate}/>);
+    render(<CreateDinghyClass onCreate={fnOnCreate} />);
     const btnCreate = screen.getByRole('button', {'name': 'Create'});
-    await user.click(btnCreate);
+    await act(async () => {
+        await user.click(btnCreate);
+    });
 
     expect(fnOnCreate).toBeCalledTimes(1);
 });
 
 it('calls the function passed in to onCreate prop with new dinghy class as parameter', async () => {
     const user = userEvent.setup();
-    const fnOnCreate = jest.fn();
+    const fnOnCreate = jest.fn((dinghyClass) => {return {'success': true}});
     
-    render(<CreateDinghyClass onCreate={fnOnCreate}/>);
+    render(<CreateDinghyClass onCreate={fnOnCreate} />);
     const btnCreate = screen.getByRole('button', {'name': 'Create'});
     const txtClassName = screen.getByLabelText('Class Name');
-    await user.type(txtClassName, 'Scorpion');
-    await user.click(btnCreate);
+    // await user.type(txtClassName, 'Scorpion');
+    await act(async () => {
+        await user.type(txtClassName, 'Scorpion');
+        await user.click(btnCreate);
+    });
 
     expect(fnOnCreate).toBeCalledWith({'name': 'Scorpion'});
 });
+
+describe('when creating a new dinghy class', () => {
+    it('clears the input on success', async () => {
+        const user = userEvent.setup();
+        const fnOnCreate = jest.fn(() => {return {'success': true}});
+        
+        render(<CreateDinghyClass onCreate={fnOnCreate} />);
+        const btnCreate = screen.getByRole('button', {'name': 'Create'});
+        const txtClassName = screen.getByLabelText('Class Name');
+        await act(async () => {
+            await user.type(txtClassName, 'Scorpion');
+            await user.click(btnCreate);
+        });
+    
+        expect(txtClassName.value).toBe('');
+    })
+    it('displays the failure message on failure', async () => {
+        const user = userEvent.setup();
+        const fnOnCreate = jest.fn(() => {return {'success': false, 'message': 'That was a bust!'}});
+        
+        render(<CreateDinghyClass onCreate={fnOnCreate} />);
+        const btnCreate = screen.getByRole('button', {'name': 'Create'});
+        await act(async () => {
+            await user.click(btnCreate);
+        });
+        const message = await screen.findByText('That was a bust!');
+    
+        expect(message).toBeInTheDocument();
+    })
+})
