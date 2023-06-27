6ba2ac2c801ce5cfa2fe1319f5e3bfe69d6fe8e0
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-06-27 14:52:37 +0100

    add DinghyRacingModel

---
+++ src/model/dinghy-racing-model.js
@@ -0,0 +1,10 @@
+const DinghyRacingModel = {
+    // Potential response codes:
+    //   Sucess: 200, 201
+    //  'Expected' failure: 400, 404, 408, 500, 503
+    createDinghyClass: function (name) {
+
+    }
+}
+
+export default DinghyRacingModel;
