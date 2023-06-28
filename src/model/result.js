a684b20937c1fa1bf7ed396a4da48244ed5073ff
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-06-28 11:58:02 +0100

    update Result type definition in model package

--- src/model/result.js
+++ src/model/result.js
@@ -1,4 +1,5 @@
 /**
  * @typedef Result
  * @property {boolean} success Indicates the success or failure of the operation
+ * @property {string} message Provides additional information on the outcome of the operation; such as the cause of failure
  */
