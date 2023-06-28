551947686975b9385415510cbdc5bc338f9c49f2
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-06-28 11:56:26 +0100

    add comments to Result type definition in controller package

--- src/controller/result.js
+++ src/controller/result.js
@@ -1,5 +1,5 @@
 /**
  * @typedef Result
- * @property {boolean} success
- * @property {string} message
+ * @property {boolean} success Indicates the success or failure of the operation
+ * @property {string} message Provides additional information on the outcome of the operation; such as the cause of failure
  */
