2e75cab8f0af4936ab9e1de6edb763e1705e6dd6
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-07-03 15:42:23 +0100

    wire DinghyRacingModel into index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import DinghyRacingModel from './model/dinghy-racing-model';
 import DinghyRacingController from './controller/dinghy-racing-controller'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const controller = new DinghyRacingController();
+const model = new DinghyRacingModel('http://localhost:8081/dinghyracing/api');
+const controller = new DinghyRacingController(model);
 
 root.render(
   <React.StrictMode>
