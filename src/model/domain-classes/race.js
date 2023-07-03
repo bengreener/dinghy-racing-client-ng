7e22602a4d903c5356375b988bc254c7805fb339
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-07-03 20:09:30 +0100

    update model domain race definition to include dinghy class property

--- src/model/domain-classes/race.js
+++ src/model/domain-classes/race.js
@@ -2,4 +2,5 @@
  * @typedef Race
  * @property {string} name Name of the race
  * @property {Date} time Time the race is scheduled to start
+ * @property {DinghyClass} dinghyClass The class of dinghy that may participate in the race
  */
