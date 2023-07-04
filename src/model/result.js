203c20a3e4e5c45af79c96b6300cf232803b824c
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-07-04 16:25:32 +0100

    add domainObject property model result to allow a domain object to be returned as the result of a model operation

--- src/model/result.js
+++ src/model/result.js
@@ -2,4 +2,5 @@
  * @typedef Result
  * @property {boolean} success Indicates the success or failure of the operation
  * @property {string} message Provides additional information on the outcome of the operation; such as the cause of failure
+ * @property {Object} domainObject The domain object returned by the operation
  */
