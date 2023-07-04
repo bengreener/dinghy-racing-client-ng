332cf338135b5f0944519882c66161f8e75d25b6
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-07-04 10:46:57 +0100

    change create race form to return form element

--- src/view/CreateRace.js
+++ src/view/CreateRace.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 function CreateRace() {
     return (
-        <>
-        <label htmlFor="race-name-input">Race Name</label>
-        <input id="race-name-input" name="name" type="text" /> {/* onChange={handleChange} value={dinghyClass.name} /> */}
-        <label htmlFor="race-time-input">Race Time</label>
-        <input id="race-time-input" name="time" type="datetime-local" /> {/*onChange={handleChange} value={dinghyClass.name} /> */}
-        <label htmlFor="race-name-input">Race Class</label>
-        <input id="race-class-input" name="class" type="text" /> {/* onChange={handleChange} value={dinghyClass.name} /> */}
-        </>
+        <form action="" method="post">
+            <label htmlFor="race-name-input">Race Name</label>
+            <input id="race-name-input" name="name" type="text" /> {/* onChange={handleChange} value={dinghyClass.name} /> */}
+            <label htmlFor="race-time-input">Race Time</label>
+            <input id="race-time-input" name="time" type="datetime-local" /> {/*onChange={handleChange} value={dinghyClass.name} /> */}
+            <label htmlFor="race-name-input">Race Class</label>
+            <input id="race-class-input" name="class" type="text" /> {/* onChange={handleChange} value={dinghyClass.name} /> */}
+        </form>
     );
 }
 
