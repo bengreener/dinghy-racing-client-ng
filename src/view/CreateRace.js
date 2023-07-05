1972df0c52d6ed8ae78ae6f155edc18500c69ed9
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-07-05 12:35:25 +0100

    update CreateRace to provide functionality required to create race

--- src/view/CreateRace.js
+++ src/view/CreateRace.js
@@ -1,14 +1,53 @@
 import React from 'react';
 
-function CreateRace() {
+function CreateRace({ onCreate }) {
+    const [race, setRace] = React.useState({'name': '', 'time': new Date().toISOString().substring(0, 16), 'dinghyClass': {'name': ''}});
+    const [result, setResult] = React.useState({'message': ''});
+
+    const clear = React.useCallback(() => {
+        setRace({'name': '', 'time': new Date().toISOString().substring(0, 16), 'dinghyClass': {'name': ''}});
+        showMessage('');
+    }, []);
+
+    React.useEffect(() => {
+        if (result && result.success) {
+            clear();
+        }
+        if (result && !result.success) {
+            showMessage(result.message);
+        }
+    }, [result, clear]);
+
+    async function handleCreate(event) {
+        event.preventDefault();
+        const domainRace = {...race, 'time': new Date(race.time)};
+        setResult(await onCreate(domainRace));
+    }
+
+    function handleChange({target}) {
+        if (target.name !== 'dinghyClass') {
+            setRace({...race, [target.name]: target.value});
+        }
+        else {
+            setRace({...race, 'dinghyClass': {'name': target.value}});
+        }
+    }
+
+    function showMessage(message) {
+        const output = document.getElementById('race-message-output');
+        output.value = message;
+    }
+
     return (
         <form action="" method="post">
             <label htmlFor="race-name-input">Race Name</label>
-            <input id="race-name-input" name="name" type="text" /> {/* onChange={handleChange} value={dinghyClass.name} /> */}
+            <input id="race-name-input" name="name" type="text" onChange={handleChange} value={race.name} />
             <label htmlFor="race-time-input">Race Time</label>
-            <input id="race-time-input" name="time" type="datetime-local" /> {/*onChange={handleChange} value={dinghyClass.name} /> */}
-            <label htmlFor="race-name-input">Race Class</label>
-            <input id="race-class-input" name="class" type="text" /> {/* onChange={handleChange} value={dinghyClass.name} /> */}
+            <input id="race-time-input" name="time" type="datetime-local" onChange={handleChange} value={race.time} />
+            <label htmlFor="race-class-input">Race Class</label>
+            <input id="race-class-input" name="dinghyClass" type="text" onChange={handleChange} value={race.dinghyClass.name} />
+            <output id="race-message-output" />
+            <button id="race-create-button" type="button" onClick={handleCreate}>Create</button>
         </form>
     );
 }
--- src/view/CreateRace.test.js
+++ src/view/CreateRace.test.js
@@ -5,6 +5,12 @@ import CreateRace from './CreateRace';
 
 it('renders', () => {
     render(<CreateRace />);
+    const inputName = screen.getByLabelText(/name/i);
+    const inputTime = screen.getByLabelText(/time/i);
+    const inputDinghyClass = screen.getByLabelText(/class/i);
+    expect(inputName).toBeInTheDocument();
+    expect(inputTime).toBeInTheDocument();
+    expect(inputDinghyClass).toBeInTheDocument();
 })
 
 it('accepts the name for a race', async () => {
@@ -23,13 +29,12 @@ it('accepts the time for the race', async () => {
     const user = userEvent.setup();
 
     render(<CreateRace />);
-    const dateRaceTime = await screen.findByLabelText('Race Time');
+    const inputTime = screen.getByLabelText(/time/i);
     await act(async () => {
-        await user.click(dateRaceTime);
-        await user.clear(dateRaceTime)
-        await user.keyboard('2020-05-12T12:30');
+        await user.clear(inputTime); // clear input to avoid errors when typing in new value
+        await user.type(inputTime, '2020-05-12T12:30');
     });
-    expect(dateRaceTime).toHaveValue('2020-05-12T12:30');
+    expect(inputTime).toHaveValue('2020-05-12T12:30');
 })
 
 it('accepts the class for the race', async () => {
@@ -42,4 +47,74 @@ it('accepts the class for the race', async () => {
     });
     
     expect(txtRaceClass).toHaveValue('Comet');
+})
+
+it('calls the function passed in to onCreate prop', async () => {
+    const user = userEvent.setup();
+    const fnOnCreate = jest.fn((race) => {return {'success': true}});
+    
+    render(<CreateRace onCreate={fnOnCreate} />);
+    const btnCreate = screen.getByRole('button', {'name': 'Create'});
+    await act(async () => {
+        await user.click(btnCreate);
+    });
+
+    expect(fnOnCreate).toBeCalledTimes(1);
+});
+
+it('calls the function passed in to onCreate prop with new race as parameter', async () => {
+    const user = userEvent.setup();
+    const fnOnCreate = jest.fn((race) => {return {'success': true}});
+    
+    render(<CreateRace onCreate={fnOnCreate} />);
+    const inputName = screen.getByLabelText(/name/i);
+    const inputTime = screen.getByLabelText(/time/i);
+    const inputDinghyClass = screen.getByLabelText(/class/i);
+    const btnCreate = screen.getByRole('button', {'name': 'Create'});
+    await act(async () => {
+        await user.type(inputName, 'Scorpion A');
+        await user.clear(inputTime); // clear input to avoid errors when typing in new value
+        await user.type(inputTime, '2020-05-12T12:30');
+        await user.type(inputDinghyClass, 'Scorpion');
+        await user.click(btnCreate);
+    });
+
+    expect(fnOnCreate).toBeCalledWith({'name': 'Scorpion A', 'time': new Date('2020-05-12T12:30'), 'dinghyClass': {'name': 'Scorpion'}});
+});
+
+describe('when creating a new dinghy class', () => {
+    it('clears the input on success', async () => {
+        const user = userEvent.setup();
+        const fnOnCreate = jest.fn(() => {return {'success': true}});
+        
+        render(<CreateRace onCreate={fnOnCreate} />);
+        const inputName = screen.getByLabelText(/name/i);
+        const inputTime = screen.getByLabelText(/time/i);
+        const inputDinghyClass = screen.getByLabelText(/class/i);
+        const btnCreate = screen.getByRole('button', {'name': 'Create'});
+        await act(async () => {
+            await user.type(inputName, 'Scorpion A');
+            await user.type(inputTime, '2020-05-12T12:30');
+            await user.type(inputDinghyClass, 'Scorpion');
+            await user.click(btnCreate);
+        });
+    
+        expect(inputName).toHaveValue('');
+        // expect(inputTime).toHaveValue('');
+        expect(inputTime).toHaveValue(new Date().toISOString().substring(0, 16));
+        expect(inputDinghyClass).toHaveValue('');
+    })
+    it('displays the failure message on failure', async () => {
+        const user = userEvent.setup();
+        const fnOnCreate = jest.fn(() => {return {'success': false, 'message': 'That was a bust!'}});
+        
+        render(<CreateRace onCreate={fnOnCreate} />);
+        const btnCreate = screen.getByRole('button', {'name': 'Create'});
+        await act(async () => {
+            await user.click(btnCreate);
+        });
+        const message = await screen.findByText('That was a bust!');
+    
+        expect(message).toBeInTheDocument();
+    })
 })
