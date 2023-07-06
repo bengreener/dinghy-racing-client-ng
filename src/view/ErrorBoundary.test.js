6b2c3c0ccfe5a16fc8c90363182acbb817855c5f
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-07-06 11:49:00 +0100

    remove console logging of errors in test that throws on purpose

--- src/view/ErrorBoundary.test.js
+++ src/view/ErrorBoundary.test.js
@@ -1,6 +1,15 @@
 import { render, screen, prettyDOM } from '@testing-library/react';
 import ErrorBoundary from './ErrorBoundary';
 
+// beforeEach(() => {
+//     jest.spyOn(console, 'error');
+//     console.error.mockImplementation(() => {});
+// })
+
+// afterEach(() => {
+//     console.error.mockRestore();
+// })
+
 describe('when there is no error', () => {
     it('renders children', () => {
         render(<ErrorBoundary children={<h1>Child Prop</h1>}/>);
@@ -10,6 +19,10 @@ describe('when there is no error', () => {
 
 describe('when there is an error', () => {
     it('displays the error message', () => {
+        // an error is expected to be thrown so mock out console logging of errors so as not to clutter up console
+        jest.spyOn(console, 'error');
+        console.error.mockImplementation(() => {});
+
         const ThrowError = () => {
             throw new SyntaxError('Oops!');
         }
@@ -23,5 +36,8 @@ describe('when there is an error', () => {
         expect(screen.getByText('Error')).toBeInTheDocument();
         expect(screen.getByText('SyntaxError')).toBeInTheDocument();
         expect(screen.getByText('Oops!')).toBeInTheDocument();
-    })
+
+        // restore console logging for errors
+        console.error.mockRestore();
+    });
 })
