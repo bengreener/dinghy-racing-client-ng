176750c4dc282485cc3df1bbcb52475bd491d43e
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-07-06 19:45:15 +0100

    add ModelContext

--- src/test-utilities/custom-renders.js
+++ src/test-utilities/custom-renders.js
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react';
-import { ModelContext } from '../App';
+import { ModelContext } from '../view/ModelContext';
+
 
 export const customRender = (ui, model, {...renderOptions} = {}) => {
     return render(
