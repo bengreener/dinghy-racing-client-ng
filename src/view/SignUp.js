62c773d811999077cfa981bcfd3ca7596e1e8b2d
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-07-28 10:18:23 +0100

    add data entry and create behaviour to SignUp form

--- src/view/SignUp.js
+++ src/view/SignUp.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 
 /**
  * Form for signing up to a race
@@ -6,8 +7,43 @@ import React from 'react';
  * @param {Race} props.race The race to sign up to
  * @returns {HTMLFormElement}
  */
-function SignUp({ race }) {
+function SignUp({ race, onCreate }) {
+    const [competitor, setCompetitor] = useState({'name': '', 'url': ''});
+    const [dinghy, setDinghy] = useState({'sailNumber': '', 'dinghyClass': {'name': '', 'url': ''}, 'url': ''});
+    const [result, setResult] = useState({'message': ''});
+
+    const clear = React.useCallback(() => {
+        setCompetitor({'name': ''});
+        setDinghy({'sailNumber': '', 'dinghyClass': {'name': '', 'url': ''}, 'url': ''});
+        showMessage('');
+    }, []);
+
+    React.useEffect(() => {
+        if (result && result.success) {
+            clear();
+        }
+        if (result && !result.success) {
+            showMessage(result.message);
+        }
+    }, [result, clear]);
     
+    function handleChange({target}) {
+        if (target.name === 'sailNumber') {
+            setDinghy({...dinghy, 'sailNumber': target.value});
+        }
+        if (target.name === 'competitor') {
+            setCompetitor({...competitor, 'name': target.value});
+        }
+        if (target.name === 'dinghyClass') {
+            setDinghy({...dinghy, 'dinghyClass': {...dinghy.dinghyClass, 'name': target.value}});
+        }
+    }
+
+    async function handleCreate(event) {
+        event.preventDefault();
+        setResult(await onCreate(race, competitor, dinghy));
+    }
+
     function dinghyClass(race) {
         let dinghyClass = null;
     
@@ -15,20 +51,31 @@ function SignUp({ race }) {
             dinghyClass = (
                 <>
                     <label htmlFor="dinghy-class-input">Dinghy Class</label>
-                    <input id="dinghy-class-input" name="dinghyClass" type="text" />
+                    <input id="dinghy-class-input" name="dinghyClass" type="text" onChange={handleChange} value={dinghy.dinghyClass.name} />
                 </>
-            )
+            );
+        }
+        // if race has a specified dinghy class then set for selected dinghy as well
+        else if (dinghy.dinghyClass.name === '') {
+            setDinghy({...dinghy, 'dinghyClass': race.dinghyClass});
         }
         return dinghyClass;
+    }
+
+    function showMessage(message) {
+        const output = document.getElementById('entry-message-output');
+        output.value = message;
     }
 
     return (
         <form action="" method="get">
             <label htmlFor="competitor-input">Competitor's Name</label>
-            <input id="competitor-input" name="competitor" type="text" />
+            <input id="competitor-input" name="competitor" type="text" onChange={handleChange} value={competitor.name} />
             {dinghyClass(race)}
             <label htmlFor="sail-number-input">Sail Number</label>
-            <input id="sail-number-input" name="sailNumber" type="text" />
+            <input id="sail-number-input" name="sailNumber" type="text" onChange={handleChange} value={dinghy.sailNumber}/>
+            <output id="entry-message-output" />
+            <button id="entry-create-button" type="button" onClick={handleCreate} >Create</button>
         </form>
     )
 }
--- src/view/SignUp.test.js
+++ src/view/SignUp.test.js
@@ -1,10 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import SignUp from './SignUp';
-import { raceScorpionA, raceNoClass } from '../model/__mocks__/test-data';
+import { raceScorpionA, raceNoClass, dinghyClassScorpion } from '../model/__mocks__/test-data';
 
 it('renders', () => {
-    render(<SignUp race={raceScorpionA}/>)
-})
+    render(<SignUp race={raceScorpionA}/>);
+    const inputCompetitor = screen.getByLabelText(/competitor/i);
+    const inputSailNumber = screen.getByLabelText(/sail/i);
+    const btnCreate = screen.getByRole('button', {'name': 'Create'});
+    expect(inputCompetitor).toBeInTheDocument();
+    expect(inputSailNumber).toBeInTheDocument();
+    expect(btnCreate).toBeInTheDocument();
+});
 
 describe('when race has a specified dinghyClass', () => {
     it('requests competitor name and dinghy sail number', () => {
@@ -15,7 +22,7 @@ describe('when race has a specified dinghyClass', () => {
         expect(inputCompetitor).toBeInTheDocument();
         expect(inputSailNumber).toBeInTheDocument();
     })
-})
+});
 
 describe('when race is a handicap race ', () => {
     it('requests competitor name and dinghy class and sail number', () => {
@@ -28,4 +35,132 @@ describe('when race is a handicap race ', () => {
         expect(inputDingyClass).toBeInTheDocument();
         expect(inputSailNumber).toBeInTheDocument();
     })
-})
+});
+
+it('when competitor name is entered then displays name', async () => {
+    const user = userEvent.setup();
+    render(<SignUp race={raceScorpionA}/>);
+
+    const inputCompetitor = await screen.findByLabelText(/competitor/i);
+    await act(async () => {
+        await user.type(inputCompetitor, 'Bob Dunn');
+    });
+    expect(inputCompetitor).toHaveValue('Bob Dunn');
+});
+
+it('when sail number is entered then displays sail number', async () => {
+    const user = userEvent.setup();
+    render(<SignUp race={raceScorpionA}/>);
+
+    const inputSailNumber = await screen.findByLabelText(/sail/i);
+    await act(async () => {
+        await user.type(inputSailNumber, '1234');
+    });
+    expect(inputSailNumber).toHaveValue('1234');
+});
+
+it('when dinghy class is entered then displays dinghy class', async () => {
+    const user = userEvent.setup();
+    render(<SignUp race={raceNoClass}/>);
+
+    const inputDinghyClass = await screen.findByLabelText(/class/i);
+    await act(async () => {
+        await user.type(inputDinghyClass, 'Scorpion');
+    });
+    expect(inputDinghyClass).toHaveValue('Scorpion');
+});
+
+it('when create button is clicked and race has a specified dinghy class then function passed to onCreate prop is called with values entered into form and dinghy dinghy class set per race', async () => {
+    const onCreateMock = jest.fn();
+    const user = userEvent.setup();
+
+    render(<SignUp race={raceScorpionA} onCreate={onCreateMock} />);
+    const btnCreate = screen.getByRole('button', {'name': 'Create'});
+    const inputCompetitor = await screen.findByLabelText(/competitor/i);
+    const inputSailNumber = await screen.findByLabelText(/sail/i);
+
+    await act(async () => {
+        await user.type(inputCompetitor, 'Bob Dunn');
+        await user.type(inputSailNumber, '1234');
+        await user.click(btnCreate);
+    });
+
+    expect(onCreateMock).toHaveBeenCalledWith(raceScorpionA, {'name': 'Bob Dunn', 'url': ''}, {'sailNumber': '1234', 'dinghyClass': dinghyClassScorpion, 'url': ''});
+});
+
+it('when create button is clicked and race does not have a specified dinghy class then function passed to onCreate prop is called with values entered into form', async () => {
+    const onCreateMock = jest.fn();
+    const user = userEvent.setup();
+
+    render(<SignUp race={raceNoClass} onCreate={onCreateMock} />);
+    const btnCreate = screen.getByRole('button', {'name': 'Create'});
+    
+    const inputCompetitor = await screen.findByLabelText(/competitor/i);
+    const inputSailNumber = await screen.findByLabelText(/sail/i);
+    const inputDinghyClass = await screen.findByLabelText(/class/i);
+
+    await act(async () => {
+        await user.type(inputCompetitor, 'Bob Dunn');
+        await user.type(inputSailNumber, '1234');
+        await user.type(inputDinghyClass, 'Graduate');
+        await user.click(btnCreate);
+    });
+    
+    expect(onCreateMock).toHaveBeenCalledWith(raceNoClass, {'name': 'Bob Dunn', 'url': ''}, {'sailNumber': '1234', 'dinghyClass': {'name': 'Graduate', 'url': ''}, 'url': ''});
+});
+
+it('when create button is clicked if onCreate function returns success then form is cleared', async () => {
+    const onCreateMock = jest.fn(() => {
+        return Promise.resolve({'success': true});
+    });
+    const user = userEvent.setup();
+
+    render(<SignUp race={raceNoClass} onCreate={onCreateMock} />);
+    
+    const inputCompetitor = await screen.findByLabelText(/competitor/i);
+    const inputSailNumber = await screen.findByLabelText(/sail/i);
+    const inputDinghyClass = await screen.findByLabelText(/class/i);
+
+    const btnCreate = screen.getByRole('button', {'name': 'Create'});
+    await act(async () => {
+        await user.type(inputCompetitor, 'Bob Dunn');
+        await user.type(inputSailNumber, '1234');
+        await user.type(inputDinghyClass, 'Scorpion');
+    });
+    expect(inputCompetitor).toHaveValue('Bob Dunn');
+    expect(inputSailNumber).toHaveValue('1234');
+    expect(inputDinghyClass).toHaveValue('Scorpion');
+    
+    await act(async () => {
+        await user.click(btnCreate);
+    });
+    expect(inputCompetitor).toHaveValue('');
+    expect(inputSailNumber).toHaveValue('');
+    expect(inputDinghyClass).toHaveValue('');
+});
+
+it('when create button is clicked if onCreate function returns failure then failure message is displayed and entered values remain on form', async () => {
+    const onCreateMock = jest.fn(() => {
+        return Promise.resolve({'success': false, 'message': 'Something went wrong'});
+    });
+    const user = userEvent.setup();
+
+    render(<SignUp race={raceNoClass} onCreate={onCreateMock} />);
+    
+    const inputCompetitor = await screen.findByLabelText(/competitor/i);
+    const inputSailNumber = await screen.findByLabelText(/sail/i);
+    const inputDinghyClass = await screen.findByLabelText(/class/i);
+
+    const btnCreate = screen.getByRole('button', {'name': 'Create'});
+    await act(async () => {
+        await user.type(inputCompetitor, 'Bob Dunn');
+        await user.type(inputSailNumber, '1234');
+        await user.type(inputDinghyClass, 'Scorpion');
+        await user.click(btnCreate);
+    });
+    expect(inputCompetitor).toHaveValue('Bob Dunn');
+    expect(inputSailNumber).toHaveValue('1234');
+    expect(inputDinghyClass).toHaveValue('Scorpion');
+    const message = await screen.findByText('Something went wrong');
+    expect(message).toBeInTheDocument();
+});
