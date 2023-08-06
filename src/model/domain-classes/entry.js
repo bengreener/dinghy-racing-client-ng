f67bfd0d58fd52c3e62659c1fd929ce52fcd0834
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-08-06 09:40:05 +0100

    add Entry to model domain classes

---
+++ src/model/domain-classes/entry.js
@@ -0,0 +1,6 @@
+/**
+ * @typedef Entry
+ * @property {Race} race The race entered
+ * @property {Competitor} competitor The competitor entering the race
+ * @property {Dinghy} dinghy The dinghy that will be sailed in the race
+ */
