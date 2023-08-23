35b3e304a5c8cef85a81b51d1a40ba4ce201c66a
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-08-23 09:44:26 +0100

    update RaceHeaderView start clock and show elapsed time countdown

--- src/view/RaceHeaderView.js
+++ src/view/RaceHeaderView.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useCallback, useContext, useState } from 'react';
+import Clock from '../model/domain-classes/clock';
+import ControllerContext from './ControllerContext';
 
-function RaceHeaderView() {
+function RaceHeaderView({ race }) {
+    const controller = useContext(ControllerContext);
+    const [clock] = useState(new Clock());
+    const [remainingTime, setRemainingTime] = useState(race.duration);
+    
+    const tickHandler = useCallback(() => {
+        setRemainingTime(race.duration - clock.getElapsedTime());
+    }, [clock, race.duration]);
+    
+    clock.addTickHandler(tickHandler);
+
+    function handleStartRaceClick() {
+        controller.startRace(race);
+        clock.start();
+    }
+
+    function handleStopRaceClick() {
+        clock.stop();
+    }
 
     return (
         <>
+        <label>{race.name}</label>
+        <label htmlFor="race-duration">Duration</label>
+        <output id="race-duration">{Clock.formatDuration(race.duration)}</output>
+        <label htmlFor="race-duration-remaining">Remaining</label>
+        <output id="race-duration-remaining">{Clock.formatDuration(remainingTime)}</output>
+        <button id="race-start-button" onClick={handleStartRaceClick}>Start Race</button>
+        <button id="race-stop-button" onClick={handleStopRaceClick}>Stop Race</button>
         </>
     );
 }
--- src/view/RaceHeaderView.test.js
+++ src/view/RaceHeaderView.test.js
@@ -1,6 +1,91 @@
-import { render } from '@testing-library/react';
+import { customRender } from '../test-utilities/custom-renders';import userEvent from '@testing-library/user-event';
+import { act, render, screen, waitFor } from '@testing-library/react';
 import RaceHeaderView from './RaceHeaderView';
+import DinghyRacingModel from '../model/dinghy-racing-model';
+import DinghyRacingController from '../controller/dinghy-racing-controller';
+import { rootURL, raceScorpionA } from '../model/__mocks__/test-data';
 
 it('renders', () => {
-    render(<RaceHeaderView />);
+    render(<RaceHeaderView race={ raceScorpionA }/>);
 });
+
+it('displays race name', () => {
+    render(<RaceHeaderView race={ raceScorpionA } />);
+    expect(screen.getByText(/scorpion a/i)).toBeInTheDocument();
+});
+
+it('displays initial race duration', () => {
+    render(<RaceHeaderView race={ raceScorpionA } />);
+    expect(screen.getByLabelText(/duration/i)).toHaveValue('00:45:00');
+});
+
+it('displays remaining race duration', () => {
+    render(<RaceHeaderView race={ raceScorpionA } />);
+    expect(screen.getByLabelText(/remaining/i)).toHaveValue('00:45:00');
+});
+
+it('displays start race button', () => {
+    render(<RaceHeaderView race={ raceScorpionA } />);
+    expect(screen.getByText(/start/i)).toBeInTheDocument();
+});
+
+it('displays stop race button', () => {
+    render(<RaceHeaderView race={ raceScorpionA } />);
+    expect(screen.getByText(/stop/i)).toBeInTheDocument();
+});
+
+it('starts the selected race', async () => {
+    const user = userEvent.setup();
+    const model = new DinghyRacingModel(rootURL);
+    const controller = new DinghyRacingController(model);
+    const controllerStartRaceSpy = jest.spyOn(controller, 'startRace');
+
+    customRender(<RaceHeaderView race={ raceScorpionA } />, model, controller);
+    
+    const buttonStart = screen.getByText(/start/i);
+    await user.click(buttonStart);
+    expect(controllerStartRaceSpy).toBeCalledWith(raceScorpionA);
+});
+
+describe('when a race is started', () => {
+    it('updates the remaining time field to show the time remaining', async () => {
+        const user = userEvent.setup();
+        const model = new DinghyRacingModel(rootURL);
+        const controller = new DinghyRacingController(model);
+        jest.spyOn(controller, 'startRace');
+
+        customRender(<RaceHeaderView race={ raceScorpionA } />, model, controller);
+
+        const buttonStart = screen.getByText(/start/i);
+        const outputRemaining = screen.getByLabelText(/remaining/i);
+
+        await user.click(buttonStart);
+        await waitFor(() => expect(outputRemaining).toHaveValue('00:44:58'), {'timeout': 3000});
+    });
+    // Have not figured out to test a value has not changed after a period of time. Can revisit when need ti to do something other than stopping clock 
+    it('stops the selected race', async () => {
+        // const user = userEvent.setup();
+        // const model = new DinghyRacingModel(rootURL);
+        // const controller = new DinghyRacingController(model);
+        // jest.spyOn(controller, 'startRace');
+        // const clockSpy = jest.spyOn(Clock, 'formatDuration')
+
+        // customRender(<RaceHeaderView race={ raceScorpionA } />, model, controller);
+
+        // const buttonStart = screen.getByText(/start/i);
+        // const buttonStop = screen.getByText(/stop/i);
+        // const outputRemaining = screen.getByLabelText(/remaining/i);
+        // await user.click(buttonStart);
+        // await setTimeout(() => {
+        //     user.click(buttonStop);
+        // }, 1000);
+        // await setTimeout(async () => {
+        //     expect(outputRemaining).toHaveValue('00:44:59');
+        // }, 2000);
+        // await waitFor(() => {
+
+        //     expect(clockSpy).toHav
+        // })
+        // screen.debug();
+    });
+})
