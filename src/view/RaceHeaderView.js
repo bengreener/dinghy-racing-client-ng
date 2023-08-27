548f874cac336f79fc3bfc447f293879675c848e
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-08-27 22:54:23 +0100

    reset form to div in RaceHeaderView to remove issue with form submission on button click

--- src/view/RaceHeaderView.js
+++ src/view/RaceHeaderView.js
@@ -30,7 +30,7 @@ function RaceHeaderView({ race }) {
     }
 
     return (
-        <form>
+        <div>
         <label>{race.name}</label>
         <label htmlFor={'race-duration-' + race.name.replace(/ /g, '-').toLowerCase()}>Duration</label>
         <output id={'race-duration-' + race.name.replace(/ /g, '-').toLowerCase()}>{Clock.formatDuration(race.duration)}</output>
@@ -38,7 +38,7 @@ function RaceHeaderView({ race }) {
         <output id={'race-duration-remaining-' + race.name.replace(/ /g, '-').toLowerCase()}>{Clock.formatDuration(remainingTime)}</output>
         <button id="race-start-button" onClick={handleStartRaceClick}>Start Race</button>
         <button id="race-stop-button" onClick={handleStopRaceClick}>Stop Race</button>
-        </form>
+        </div>
     );
 }
 
