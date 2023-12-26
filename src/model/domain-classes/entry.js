e0a428f2279155422d2c2b65231b7219a7f7241a
Ben Greener <53060452+bengreener@users.noreply.github.com>
2023-12-26 13:41:06 +0000

    add crew to entry type defintition

--- src/model/domain-classes/entry.js
+++ src/model/domain-classes/entry.js
@@ -2,6 +2,7 @@
  * @typedef Entry
  * @property {Race} race The race entered
  * @property {Competitor} helm The competitor entering the race as helm
+ * @property {Competitor} crew The copetitor entering the race as crew
  * @property {Dinghy} dinghy The dinghy that will be sailed in the race
  * @property {string} url The URL to the remote resource
  * @property {Array<Lap>} laps The laps for this entry in the race
