e3733a39c9c9ca5b7bb63d39a66fa1498473b20c
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-01-04 14:45:40 +0000

    class with crew add helm add crew add dinghy signup button test

--- src/view/SignUp_2.test.js
+++ src/view/SignUp_2.test.js
@@ -1598,8 +1598,22 @@ describe('when race for dinghy class with crew', () => {
     });
 
 	describe('when neither helm nor dinghy nor crew exist', () => {
-        it('displays create helm & dinghy & crew & sign-up button', () => {
-
+        it('displays create helm & dinghy & crew & sign-up button', async () => {
+            const user = userEvent.setup();
+            customRender(<SignUp race={raceScorpionA}/>, model, controller);
+            const inputHelm = await screen.findByLabelText(/helm/i);
+            const inputSailNumber = await screen.findByLabelText(/sail/i);
+            const inputCrew = await screen.findByLabelText(/crew/i);
+            await act(async () => {
+                await user.type(inputHelm, 'Not There');
+            });
+            await act(async () => {
+                await user.type(inputSailNumber, 'xyz');
+            });
+            await act(async () => {
+                await user.type(inputCrew, 'Pop Off');
+            });
+            expect(screen.getByRole('button', {'name': /add helm & crew & dinghy & sign-up/i}));
         });
 		
 		describe('when create button clicked', () => {
