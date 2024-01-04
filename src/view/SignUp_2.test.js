cc396bd610e0f1643e5860773639fdf4362caeff
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-01-04 05:59:58 +0000

    class with crew add dinghy failed

--- src/view/SignUp_2.test.js
+++ src/view/SignUp_2.test.js
@@ -811,8 +811,33 @@ describe('when race for dinghy class with crew', () => {
             });
 			
 			describe('when dinghy not created', () => {
-				it('displays failure message and entered values remain on form', () => {
-
+				it('displays failure message and entered values remain on form', async () => {
+                    const createDinghySpy = jest.spyOn(controller, 'createDinghy').mockImplementation(() => {
+                        return Promise.resolve({'success': false, 'message': 'Dinghy not created'});
+                    });
+                    const user = userEvent.setup();
+                    customRender(<SignUp race={raceScorpionA}/>, model, controller);
+                    const inputHelm = await screen.findByLabelText(/helm/i);
+                    const inputSailNumber = await screen.findByLabelText(/sail/i);
+                    const inputCrew = await screen.findByLabelText(/crew/i);
+                    await act(async () => {
+                        await user.type(inputHelm, 'Chris Marshall');
+                    });
+                    await act(async () => {
+                        await user.type(inputSailNumber, 'xyz');
+                    });
+                    await act(async () => {
+                        await user.type(inputCrew, 'Lou Screw');
+                    });
+                    const createButton = screen.getByRole('button', {'name': /add dinghy & sign-up/i});
+                    await act(async () => {
+                        await user.click(createButton);
+                    });
+                    expect(createDinghySpy).toHaveBeenCalledWith({'sailNumber': 'xyz', 'dinghyClass': dinghyClassScorpion, 'url': ''});
+                    expect(screen.getByText('Dinghy not created')).toBeInTheDocument();
+                    expect(inputHelm).toHaveValue('Chris Marshall');
+                    expect(inputSailNumber).toHaveValue('xyz');
+                    expect(inputCrew).toHaveValue('Lou Screw');
                 });
             });
 
