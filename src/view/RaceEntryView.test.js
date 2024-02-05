dc2571c3ede1766c8b79a94741bcf643f07159a6
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-02-05 22:53:31 +0000

    add pointer handlers to support touch input

--- src/view/RaceEntryView.test.js
+++ src/view/RaceEntryView.test.js
@@ -4,6 +4,16 @@ import RaceEntryView from './RaceEntryView';
 import { entryChrisMarshallScorpionA1234 } from '../model/__mocks__/test-data';
 import DinghyRacingModel from '../model/dinghy-racing-model';
 
+beforeEach(() => {
+    jest.useFakeTimers();
+    jest.spyOn(global, 'setTimeout');
+});
+
+afterEach(() => {
+    jest.runOnlyPendingTimers();
+    jest.useRealTimers();
+});
+
 it('renders', () => {
     const tableBody = document.createElement('tbody');
     render(<RaceEntryView entry={entryChrisMarshallScorpionA1234} />, {container: document.body.appendChild(tableBody)});
@@ -12,7 +22,7 @@ it('renders', () => {
 });
 
 it('displays lap times', async () => {
-    const user = userEvent.setup();
+    const user = userEvent.setup({advanceTimers: jest.advanceTimersByTime});
     const entry = {...entryChrisMarshallScorpionA1234, laps: [{'number': 1, 'time': 1234}]};
     const tableBody = document.createElement('tbody');
     render(<RaceEntryView entry={entry} />, {container: document.body.appendChild(tableBody)});
@@ -20,7 +30,7 @@ it('displays lap times', async () => {
 });
 
 it('calls addLap callback with entry', async () => {
-    const user = userEvent.setup();
+    const user = userEvent.setup({advanceTimers: jest.advanceTimersByTime});
     const entry = {...entryChrisMarshallScorpionA1234};
     const addLapCallback = jest.fn((e) => {entry.laps.push({'number': 1, 'time': 1234})});
     const tableBody = document.createElement('tbody');
@@ -33,7 +43,7 @@ it('calls addLap callback with entry', async () => {
 });
 
 it('calls removeLap callback with entry', async () => {
-    const user = userEvent.setup();
+    const user = userEvent.setup({advanceTimers: jest.advanceTimersByTime});
     const entry = {...entryChrisMarshallScorpionA1234};
     const removeLapCallback = jest.fn((e) => {entry.laps.pop()});
     const tableBody = document.createElement('tbody');
@@ -47,7 +57,7 @@ it('calls removeLap callback with entry', async () => {
 });
 
 it('when secondary mouse button is clicked accepts a new lap time input in the last field of the row', async () => {
-    const user = userEvent.setup();
+    const user = userEvent.setup({advanceTimers: jest.advanceTimersByTime});
     const entry = {...entryChrisMarshallScorpionA1234, 'laps': [{...DinghyRacingModel.lapTemplate(), number: 1, time: 1000}, {...DinghyRacingModel.lapTemplate(), number: 2, time: 2000}, {...DinghyRacingModel.lapTemplate(), number: 3, time: 3000}]};
     const tableBody = document.createElement('tbody');
     render(<RaceEntryView entry={entry} />, {container: document.body.appendChild(tableBody)});
@@ -63,7 +73,7 @@ it('when secondary mouse button is clicked accepts a new lap time input in the l
 
 describe('when editing a lap time', () => {
     it('does not add new lap when primary button clicked', async () => { 
-        const user = userEvent.setup();
+        const user = userEvent.setup({advanceTimers: jest.advanceTimersByTime});
         const entry = {...entryChrisMarshallScorpionA1234, 'laps': [{...DinghyRacingModel.lapTemplate(), number: 1, time: 1000}, {...DinghyRacingModel.lapTemplate(), number: 2, time: 2000}, {...DinghyRacingModel.lapTemplate(), number: 3, time: 3000}]};
         const addLapCallback = jest.fn();
         const tableBody = document.createElement('tbody');
@@ -79,7 +89,7 @@ describe('when editing a lap time', () => {
         expect(addLapCallback).not.toHaveBeenCalled();
     });
     it('does not remove last lap when ctrl+primary button clicked', async () => {
-        const user = userEvent.setup();
+        const user = userEvent.setup({advanceTimers: jest.advanceTimersByTime});
         const entry = {...entryChrisMarshallScorpionA1234};
         const removeLapCallback = jest.fn((e) => {entry.laps.pop()});
         const tableBody = document.createElement('tbody');
@@ -96,7 +106,7 @@ describe('when editing a lap time', () => {
         expect(removeLapCallback).not.toHaveBeenCalled();
     });
     it('updates lap with new time supplied', async () => {
-        const user = userEvent.setup();
+        const user = userEvent.setup({advanceTimers: jest.advanceTimersByTime});
         const entry = {...entryChrisMarshallScorpionA1234, 'laps': [{...DinghyRacingModel.lapTemplate(), number: 1, time: 1000}, {...DinghyRacingModel.lapTemplate(), number: 2, time: 2000}, {...DinghyRacingModel.lapTemplate(), number: 3, time: 3000}]};
         const tableBody = document.createElement('tbody');
         const updateLapCallback = jest.fn((entry, value) => {});
@@ -112,5 +122,62 @@ describe('when editing a lap time', () => {
             await user.keyboard('{Enter}');
         });
         expect(updateLapCallback).toBeCalledWith(entry, 15678);
+    });
+});
+
+describe('when user taps row', () => {
+    it('calls addLap callback with entry', async () => {
+        const user = userEvent.setup({advanceTimers: jest.advanceTimersByTime});
+        const entry = {...entryChrisMarshallScorpionA1234};
+        const addLapCallback = jest.fn((e) => {entry.laps.push({'number': 1, 'time': 1234})});
+        const tableBody = document.createElement('tbody');
+        render(<RaceEntryView entry={entry} addLap={addLapCallback} />, {container: document.body.appendChild(tableBody)});
+        const SMScorp1234entry = screen.getByText(/scorpion 1234 chris marshall/i);
+        await act(async () => {
+            await user.pointer([{keys: '[TouchA]', target: SMScorp1234entry}]);
+        });
+        expect(addLapCallback).toBeCalledWith(entry);
+    });
+});
+
+describe('when user taps and holds on row', () => {
+    it('accepts a new lap time input in the last field of the row', async () => {
+        const setTimeoutSpy = jest.spyOn(global, 'setTimeout');
+        const user = userEvent.setup({advanceTimers: jest.advanceTimersByTime});
+        const entry = {...entryChrisMarshallScorpionA1234, 'laps': [{...DinghyRacingModel.lapTemplate(), number: 1, time: 1000}, {...DinghyRacingModel.lapTemplate(), number: 2, time: 2000}, {...DinghyRacingModel.lapTemplate(), number: 3, time: 3000}]};
+        const tableBody = document.createElement('tbody');
+        render(<RaceEntryView entry={entry} />, {container: document.body.appendChild(tableBody)});
+        const SMScorp1234entry = screen.getByText(/scorpion 1234 chris marshall/i);
+        const entryCell = screen.getByText(/scorpion 1234/i);
+        const lastCell = entryCell.parentElement.lastChild;
+        await act(async () => {
+            await user.pointer({target: SMScorp1234entry, keys: '[TouchA>]'});
+        });
+        act(() => {
+            jest.advanceTimersByTime(500);
+        });
+        await user.clear(lastCell.lastChild);
+        await user.type(lastCell.lastChild, '15678');
+        expect(lastCell.lastChild).toHaveValue(15678);
+    });
+});
+
+// may not be possible to test this due to a current issue in @testing-library/user-event with generating pointer move event 
+// https://github.com/testing-library/user-event/issues/1047
+describe('when user swipes left on row', () => {
+    xit('calls removeLap callback with entry', async () => {
+        const user = userEvent.setup({advanceTimers: jest.advanceTimersByTime});
+        const entry = {...entryChrisMarshallScorpionA1234, 'laps': [{...DinghyRacingModel.lapTemplate(), number: 1, time: 1000}, {...DinghyRacingModel.lapTemplate(), number: 2, time: 2000}, {...DinghyRacingModel.lapTemplate(), number: 3, time: 3000}]};
+        const removeLapCallback = jest.fn((e) => {entry.laps.pop()});
+        const tableBody = document.createElement('tbody');
+        render(<RaceEntryView entry={entry} removeLap={removeLapCallback} />, {container: document.body.appendChild(tableBody)});
+        const SMScorp1234entry = screen.getByText(/scorpion 1234 chris marshall/i);
+        const position1 = SMScorp1234entry.parentElement.children[3];
+        const position2 = SMScorp1234entry.parentElement.children[2];
+        await act(async () => {
+            await user.pointer([{target: position1, coords: {x: 100, y: 100}, keys: '[TouchA>]'}, 
+                {target:position2, coords: {x: 80, y: 100}, keys: '[TouchA]'}, {keys: '[/TouchA]'}]);
+        });
+        expect(removeLapCallback).toBeCalledWith(entry);
     });
 });
