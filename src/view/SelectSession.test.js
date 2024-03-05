3b9d3633aac70821bb35aff54401c18e9cfba6d1
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-03-05 12:12:30 +0000

    test SelectSession displays start and end dates provided

--- src/view/SelectSession.test.js
+++ src/view/SelectSession.test.js
@@ -18,3 +18,19 @@ it('provides option to select end time for session', async () => {
     const selectSessionEnd = screen.getByLabelText(/session end/i);
     expect(selectSessionEnd).toBeInTheDocument();
 });
+
+it('displays session start provided as prop', () => {
+    const sessionStart = new Date('2024', '12', '25', 8, 30).toISOString().substring(0, 16);
+    render(<SelectSession sessionStart={sessionStart}/>);
+
+    const selectSessionStart = screen.getByLabelText(/session start/i);
+    expect(selectSessionStart).toHaveValue(sessionStart);
+});
+
+it('displays session end provided as prop', () => {
+    const sessionEnd = new Date('2024', '12', '25', 18, 0).toISOString().substring(0, 16);
+    render(<SelectSession sessionEnd={sessionEnd}/>);
+
+    const selectSessionEnd = screen.getByLabelText(/session end/i);
+    expect(selectSessionEnd).toHaveValue(sessionEnd);
+});
