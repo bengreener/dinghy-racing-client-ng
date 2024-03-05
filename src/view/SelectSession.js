0dd4deba2ddf825b30e31cb66f2d2202d5102f99
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-03-05 10:53:57 +0000

    add select session start and select session end to select session component

--- src/view/SelectSession.js
+++ src/view/SelectSession.js
@@ -1,7 +1,11 @@
-function SelectSession() {
+function SelectSession({ sessionStart, sessionEnd, handleSessionStartChange, handleSessionEndChange }) {
     return (
-        <>
-        </>
+        <div>
+            <label htmlFor="select-session-start">Session Start</label>
+            <input id="select-session-start" name="sessionStartTime" type="datetime-local" onChange={handleSessionStartChange} value={sessionStart} />
+            <label htmlFor="select-session-end">Session End</label>
+            <input id="select-session-end" name="sessionEndTime" type="datetime-local" onChange={handleSessionEndChange} value={sessionEnd} />
+        </div>
     )
 }
 
--- src/view/SelectSession.test.js
+++ src/view/SelectSession.test.js
@@ -1,6 +1,20 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import SelectSession from './SelectSession';
 
 it('renders', () => {
     render(<SelectSession />);
+});
+
+it('provides option to select start time for session', async () => {
+    render(<SelectSession />);
+
+    const selectSessionStart = screen.getByLabelText(/session start/i);
+    expect(selectSessionStart).toBeInTheDocument();
+});
+
+it('provides option to select end time for session', async () => {
+    render(<SelectSession />);
+
+    const selectSessionEnd = screen.getByLabelText(/session end/i);
+    expect(selectSessionEnd).toBeInTheDocument();
 });
