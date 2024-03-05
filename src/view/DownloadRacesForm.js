3fd8e0e3a52c7825f029d673124796d2e55f4e91
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-03-05 19:44:54 +0000

    set default time window for finding races to download results for

--- src/view/DownloadRacesForm.js
+++ src/view/DownloadRacesForm.js
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 import SelectSession from './SelectSession';
 
+/**
+ * Select races to download results for.
+ */
 function DownloadRacesForm() {
+    const [sessionStart, setSessionStart] = useState(new Date(Math.floor(Date.now() / 86400000) * 86400000 + 28800000));
+    const [sessionEnd, setSessionEnd] = useState(new Date(Math.floor(Date.now() / 86400000) * 86400000 + 64800000));
 
     return (
         <div>
             <h1>Download Races</h1>
-            <SelectSession />
+            <SelectSession sessionStart={sessionStart} sessionEnd={sessionEnd} />
         </div>
     );
 }
--- src/view/DownloadRacesForm.test.js
+++ src/view/DownloadRacesForm.test.js
@@ -13,3 +13,12 @@ it('provides option to select start time and end time for session', async () => 
     expect(selectSessionStart).toBeInTheDocument();
     expect(selectSessionEnd).toBeInTheDocument();
 });
+
+it('sets start and end time to defaults', () => {
+    const expectedStartTime = new Date(Math.floor(Date.now() / 86400000) * 86400000 + 28800000).toISOString().substring(0, 16);
+    const expectedEndTime = new Date(Math.floor(Date.now() / 86400000) * 86400000 + 64800000).toISOString().substring(0, 16);
+
+    render(<DownloadRacesForm />);
+    expect(screen.getByLabelText(/session start/i)).toHaveValue(expectedStartTime);
+    expect(screen.getByLabelText(/session end/i)).toHaveValue(expectedEndTime);
+});
