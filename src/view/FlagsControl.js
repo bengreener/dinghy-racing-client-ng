9d50834106f05dda056ad050fed39d1b253ea2cd
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-03-30 17:03:35 +0000

    create GlagsControl component

---
+++ src/view/FlagsControl.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function FlagsControl() {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default FlagsControl;
---
+++ src/view/FlagsControl.test.js
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+import FlagsControl from './FlagsControl';
+
+it('renders', () => {
+    render(<FlagsControl />);
+});
