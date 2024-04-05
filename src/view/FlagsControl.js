be4ad12590d96d25d7ffd83a3403bd6a933a0685
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-04-05 15:24:53 +0100

    remove heading from FlagsControl now provided by wrapping in collapsible container

--- src/view/FlagsControl.js
+++ src/view/FlagsControl.js
@@ -21,7 +21,6 @@ function FlagsControl({races = []}) {
 
     return (
         <div>
-            <h1>Flag Indicators</h1>
             {flags.map(flag => { return <FlagControl key={flag.name} name={flag.name} clock={flag.clock} flagStateChangeTimings={flag.flagStateChangeTimings} /> })}
         </div>
     );
