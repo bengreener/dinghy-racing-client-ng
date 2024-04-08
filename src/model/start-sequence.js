0ba9ccddf40b5e269cfc74360d97fec4a16136ee
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-04-08 11:27:49 +0100

    create StartSequence enumeration

---
+++ src/model/start-sequence.js
@@ -0,0 +1,11 @@
+/**
+ * Class providng enumeration of start sequence options
+ */
+class StartSequence {
+    static WARNINGSIGNAL = 'WARNINGSIGNAL';
+    static PREPARATORYSIGNAL = 'PREPARATORYSIGNAL';
+    static ONEMINUTE = 'ONEMINUTE';
+	static STARTINGSIGNAL = 'STARTINGSIGNAL';
+}
+
+export default StartSequence;
