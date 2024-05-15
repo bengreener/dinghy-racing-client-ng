ea92c6b82bbc83e0747e9c9869eb0025a700f23d
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-05-15 20:36:29 +0100

    remove deleted files

--- src/view/CompetitorsView.js
+++
@@ -1,9 +0,0 @@
-function CompetitorsView() {
-    return (
-        <div>
-            <h1>Competitors</h1>
-        </div>
-    );
-}
-
-export default CompetitorsView;
--- src/view/CompetitorsView.test.js
+++
@@ -1,7 +0,0 @@
-import { screen, render } from '@testing-library/react';
-import CompetitorsView from './CompetitorsView';
-
-it('renders', () => {
-    render(<CompetitorsView />);
-    expect(screen.getByRole('heading', {name: 'Competitors'})).toBeInTheDocument();
-});
