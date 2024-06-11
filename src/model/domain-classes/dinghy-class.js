595541f588b40a1115e3d15df2ba114e0d6245ff
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-06-11 16:54:43 +0100

    add portsmouth number to dinghy class

--- src/model/domain-classes/dinghy-class.js
+++ src/model/domain-classes/dinghy-class.js
@@ -18,5 +18,6 @@
  * @typedef DinghyClass
  * @property {string} name The name of the dinghy class
  * @property {integer} crewSize The number of people in the crew; including the helm
+ * @property {integer} portsmouthNumber The portsmouth number handicap for this class
  * @property {string} url The URL to the remote resource
  */
