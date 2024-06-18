734d6c37e08d0b316cc6ea24d2d62682020cea62
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-06-18 14:05:51 +0100

    create RaceTypes enumeration

---
+++ src/model/race-types.js
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2022-2024 BG Information Systems Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
+/**
+ * Class providng enumeration of race type options
+ */
+class RaceTypes {
+    static FLEET = 'FLEET';
+    static PURSUIT = 'PURSUIT';
+
+    /**
+     * Return a StartSequence stage value based on the supplied string
+     * @param {String} stage
+     */
+    static from(type) {
+        const lowerType = type?.toUpperCase();
+        switch (lowerType) {
+            case 'FLEET':
+                return RaceTypes.FLEET;
+            case 'PURSUIT':
+                return RaceTypes.PURSUIT;
+        }
+    }
+}
+
+export default RaceTypes;
