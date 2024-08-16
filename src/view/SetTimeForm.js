60f91bcaad7de5239b6479fbfce2304869a45101
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-08-16 22:24:04 +0100

    create SetTimeForm for user to provide a time value

---
+++ src/view/SetTimeForm.js
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2022-2024 BG Information Systems Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
+import { useState } from 'react';
+
+let formatOptions = {
+    timeZone: 'UTC',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+};
+
+const timeFormat = new Intl.DateTimeFormat('en-GB', formatOptions);
+
+/**
+ * Get a time value from the user
+ * @param {Object} props
+ * @param {SetTimeForm~setTime} props.setTime callback to set time
+ * @param {ModalDialog~closeDialog} [props.closeParent] call this to close a dialog containing this form
+ */
+function SetTimeForm({ setTime, closeParent }) {
+    const [timeString, setTimeString] = useState(timeFormat.format(Date.now()));
+
+    function convertTimeStringToDate(timeString) {
+        const timeArray = timeString.split(':'); // hh:mm[:ss]
+        const day = Math.floor(Date.now() / 86400000) * 86400000;
+        const hours = timeArray[0] * 3600000;
+        const minutes = timeArray[1] * 60000;
+        const seconds = timeArray[2] ? timeArray[2] * 1000 : 0;
+        return new Date(day + hours + minutes + seconds);
+    }
+
+    function handleChange({ target }) {
+        const newTime = target.value ? target.value : 0;
+        setTimeString(newTime);
+    }
+
+    function handleSetTimeButtonClick(event) {
+        event.preventDefault();
+        setTime(convertTimeStringToDate(timeString));
+        if (closeParent) {
+            closeParent();
+        }
+    }
+
+    return (
+        <form action='' method='get'>
+            <div>
+                <label htmlFor="input-time" >Enter time:</label>
+                <input id="input-time" type="time" value={timeString} step="1" onChange={handleChange} />
+            </div>
+            <div>
+                {closeParent ? <button type='button' onClick={closeParent} >Cancel</button> : null}                
+                <button type='button' onClick={handleSetTimeButtonClick}>Set Time</button>
+            </div>
+        </form>
+    )
+}
+
+export default SetTimeForm;
+
+/**
+ * Action to take when AdjustCourseForm update laps button clicked
+ * @callback SetTimeForm~setTime
+ * @param {Date} time to set
+ */
---
+++ src/view/SetTimeForm.test.js
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2022-2024 BG Information Systems Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
+import { act, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import SetTimeForm from './SetTimeForm';
+
+
+let formatOptions = {
+    timeZone: 'UTC',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+};
+const timeFormat = new Intl.DateTimeFormat('en-GB', formatOptions);
+
+it('renders', () => {
+    const testTimeString = timeFormat.format(Date.now());
+    const closeParentSpy = jest.fn();
+    render(<SetTimeForm closeParent = {closeParentSpy} />);
+    expect(screen.getByLabelText(/enter time/i)).toHaveValue(testTimeString);
+    expect(screen.getByRole('button', {name: /set time/i})).toBeInTheDocument();
+    expect(screen.getByRole('button', {name: /cancel/i})).toBeInTheDocument();
+});
+
+it('displays new value entered for time', async () => {
+    const user = userEvent.setup();
+    render(<SetTimeForm />);
+    const timeInput = screen.getByLabelText(/enter time/i);
+
+    await act(async () => {
+        await user.clear(timeInput);
+        await user.type(timeInput, '0811'); // should include seconds but JSDom Input type=time does not appear to be recognising step value to set a seconds component into control
+    });
+    expect(timeInput).toHaveValue('08:11');
+});
+
+describe('when set time button clicked', () => {
+    it('calls function passed to setTime with time entered', async () => {
+        const setTimeSpy = jest.fn();
+        const user = userEvent.setup();
+        
+        render(<SetTimeForm setTime={setTimeSpy} />);
+        
+        const timeInput = screen.getByLabelText(/enter time/i);
+        const testTime = new Date(Math.floor(Date.now() / 86400000) * 86400000 + 3600000 * 8 + 60000 * 11);
+        await act(async () => {
+            await user.clear(timeInput);
+            await user.type(timeInput, '0811'); // should include seconds but JSDom Input type=time does not appear to be recognising step value to set a seconds component into control
+        });
+        await user.click(screen.getByRole('button', {name: /set time/i}));
+        expect(setTimeSpy).toHaveBeenCalledWith(testTime);
+    });
+});
+
+describe('when contained in a modal dialog', () => {
+    describe('when update laps button clicked', () => {
+        it('closes containing dialog', async () => {
+            const setTimeSpy = jest.fn();
+            const closeParentSpy = jest.fn();
+            const user = userEvent.setup();
+            
+            render(<SetTimeForm setTime={setTimeSpy} closeParent={closeParentSpy} />);
+            
+            const timeInput = screen.getByLabelText(/enter time/i);
+            await act(async () => {
+                await user.clear(timeInput);
+                await user.type(timeInput, '0811'); // should include seconds but JSDom Input type=time does not appear to be recognising step value to set a seconds component into control
+            });
+            await user.click(screen.getByRole('button', {name: /set time/i}));
+            expect(closeParentSpy).toHaveBeenCalled();
+        });
+    });
+    it('when cancelled it closes containing dialog', async () => {
+        const closeParentSpy = jest.fn();
+        const user = userEvent.setup();
+        
+        render(<SetTimeForm closeParent={closeParentSpy} />);
+        
+        const cancelButtton = screen.getByRole('button', {'name': /cancel/i});
+        await user.click(cancelButtton);
+        expect(closeParentSpy).toBeCalledTimes(1);
+    });
+});
