09271f0bd6582c29ce8982f5e5fdcd5b189e45e8
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-12-07 18:30:50 +0000

    adjust size and styling of refresh button to better fit display

--- src/view/RaceEntriesView.js
+++ src/view/RaceEntriesView.js
@@ -280,9 +280,9 @@ function RaceEntriesView({ races }) {
                 <div className='w3-col m2'>
                     <button className='w3-btn w3-block w3-card' onClick={() => setSortOrder('forecast')}>By forecast</button>
                 </div>
-                <div className='w3-col m2'>
+                <div className='w3-col m1'>
                     <button className='w3-btn w3-block w3-card' title='refresh' onClick={handleRefreshClick}>
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M480-160q-134 0-227-93t-93-227q0-134 93-227t227-93q69 0 132 28.5T720-690v-110h80v280H520v-80h168q-32-56-87.5-88T480-720q-100 0-170 70t-70 170q0 100 70 170t170 70q77 0 139-44t87-116h84q-28 106-114 173t-196 67Z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" height=".9em" viewBox="0 -960 960 960" width=".9em" fill="#5f6368"><path d="M480-160q-134 0-227-93t-93-227q0-134 93-227t227-93q69 0 132 28.5T720-690v-110h80v280H520v-80h168q-32-56-87.5-88T480-720q-100 0-170 70t-70 170q0 100 70 170t170 70q77 0 139-44t87-116h84q-28 106-114 173t-196 67Z"/></svg>
                     </button>
                 </div>
             </div>
