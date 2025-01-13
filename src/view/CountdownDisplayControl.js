b6b9ef338dd35aab013258939f307bacbba728cf
Ben Greener <53060452+bengreener@users.noreply.github.com>
2025-01-13 11:10:47 +0000

    add CountdownDisplayControl

---
+++ src/view/CountdownDisplayControl.js
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2022-2024 BG Information Systems Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
+import Clock from '../model/domain-classes/clock';
+
+/**
+ * Display a formatted time and a message
+ * @param {Object} prop
+ * @param {Integer} prop.time
+ * @param {String} prop.message
+ */
+function CountdownDisplayControl({ time, message }) {
+
+    return (
+        <div class='countdown-display-control' >
+            <p class='clock-display'>{Clock.formatDuration(time)}</p>
+            <p>{message}</p>    
+        </div>
+    )
+}
+
+export default CountdownDisplayControl;
