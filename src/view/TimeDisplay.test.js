d65d07d29f5899ae7b5ef7cc8a20324c9db7a27c
Ben Greener <53060452+bengreener@users.noreply.github.com>
2025-07-30 10:58:37 +0100

    create TimeDisplay

---
+++ src/view/TimeDisplay.test.js
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2022-2024 BG Information Systems Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
+import TimeDisplay from './TimeDisplay';
+import { render, screen } from '@testing-library/react';
+
+jest.mock('../model/domain-classes/clock');
+
+it('renders', () => {
+    render(<TimeDisplay time={1753868502608} />);
+    expect(screen.getByText('10:41:42')).toBeInTheDocument();
+});
