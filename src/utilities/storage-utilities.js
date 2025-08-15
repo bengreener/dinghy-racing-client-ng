bf8c33888fb869b909039e8a4220bb6cfa897026
Ben Greener <53060452+bengreener@users.noreply.github.com>
2025-08-15 11:41:15 +0100

    add function to test local storage is available

---
+++ src/utilities/storage-utilities.js
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2022-2024 BG Information Systems Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
+/**
+ * Tests if local or session storage is available
+ * Thanks to Mozilla.org; https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+ * @param {String} type localStorage or sessionStorage
+ * @returns {boolean}
+ */
+function storageAvailable(type) {
+    let storage;
+    try {
+      storage = window[type];
+      const x = "__storage_test__";
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    } catch (e) {
+      return (
+        e instanceof DOMException &&
+        e.name === "QuotaExceededError" &&
+        // acknowledge QuotaExceededError only if there's something already stored
+        storage &&
+        storage.length !== 0
+      );
+    }
+  }
+  
+  export { storageAvailable }
---
+++ src/utilities/storage-utilities.test.js
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2022-2024 BG Information Systems Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
+import { storageAvailable } from './storage-utilities';
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
+
+describe('when local storage is available', () =>
+{
+    it('returns true', () => {
+        expect(storageAvailable('localStorage')).toBeTruthy();
+    })
+});
+
+describe('when session storage is available', () =>
+{
+    it('returns true', () => {
+        expect(storageAvailable('sessionStorage')).toBeTruthy();
+    })
+});
+
+describe('when local storage is not available', () =>
+{
+    it('returns false', () => {
+        jest.spyOn(Storage.prototype, 'setItem').mockImplementation(() => {
+            throw new Error();
+        });
+        expect(storageAvailable('localStorage')).toBeFalsy();
+    })
+});
+
+describe('when session storage is not available', () =>
+{
+    it('returns false', () => {
+        jest.spyOn(Storage.prototype, 'setItem').mockImplementation(() => {
+            throw new Error();
+        });
+        expect(storageAvailable('sessionStorage')).toBeFalsy();
+    })
+});
