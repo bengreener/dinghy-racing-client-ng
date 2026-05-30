893c19b3b0157a60811fe69db597a5895cff8b19
Ben Greener <53060452+bengreener@users.noreply.github.com>
2026-05-30 17:16:17 +0100

    adjust formatting

--- src/view/SylphView.jsx
+++ src/view/SylphView.jsx
@@ -145,7 +145,7 @@ function SylphView({model, controller}) {
                     <button key={9} type='button' className='w3-bar-item w3-btn' onClick={() => {window.location.href = window.origin + '/logout'}}>Logout</button>
                 </div>
                 <nav className='w3-row w3-bginfosys-display-bottommiddle-m1' >
-                    <button key={3} ref={enrolmentButtonRef} type='button' className='w3-third bgis-min-content w3-btn w3-card' onClick={() => showUpcomingRaces(model)}>Enrolment</button>
+                    <button key={3} ref={enrolmentButtonRef} type='button' className='w3-third w3-btn w3-card' onClick={() => showUpcomingRaces(model)}>Enrolment</button>
                     {roles.includes('ROLE_RACE_OFFICER') ? 
                         <button key={4} ref={raceStartButtonRef} type='button' className='w3-third w3-btn w3-card' onClick={showRaceStartConsole}>Race Start</button>
                         : null
