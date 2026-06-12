569eef8ea8e2f07cda8cda30ab34cf45511aca8e
Ben Greener <53060452+bengreener@users.noreply.github.com>
2026-06-12 09:11:54 +0100

    bind advanceTimersByTime to vi in tests setup to resolve bound to jest bug

--- setupTests.js
+++ setupTests.js
@@ -1,18 +1,25 @@
 //Set ESLint no-undef rule to ignore undefined variable 'global'
 /*global global */
 
+import { vi } from 'vitest';
 import { expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import * as matchers from "@testing-library/jest-dom/matchers";
 
 expect.extend(matchers);
 
+// When using Vitest with fake timers, userEvent times out because it does not advance the fake timers. Work around this by shimming the Jest global.
+// https://github.com/testing-library/user-event/pull/1304
+globalThis.jest = {
+  advanceTimersByTime: vi.advanceTimersByTime.bind(vi),
+}
+
 afterEach(() => {
   cleanup();
 });
 
 class AudioContext {
-    createMediaElementSource() {};
+  createMediaElementSource() {};
 }
 
 if (!global.AudioContext) {
