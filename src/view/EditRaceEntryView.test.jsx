15edb10e9f93edb934e31904c8277000baea4a7c
Ben Greener <53060452+bengreener@users.noreply.github.com>
2026-07-24 10:46:14 +0100

    add renders test for EditRaceEntry

--- src/view/EditRaceEntryView.test.jsx
+++ src/view/EditRaceEntryView.test.jsx
@@ -14,3 +14,36 @@
  * limitations under the License. 
  */
 
+import { render, screen } from '@testing-library/react';
+import EditRaceEntryView from './RaceEntryView';
+import { httpRootURL, wsRootURL, competitorChrisMarshallHAL, entryChrisMarshall1234ScorpionAHAL, 
+    dinghy1234HAL, dinghyClassScorpionHAL, raceScorpionAHAL,
+    signedUpChrisMarshallDinghy1234ScorpionAHAL } from '../model/__mocks__/test-data';
+import SylphModel from '../model/sylph-model';
+import Competitor from '../model/competitor';
+import Collection from '../model/collection';
+import Dinghy from '../model/dinghy';
+import DinghyClass from '../model/dinghy-class';
+import Entry from '../model/entry';
+import DirectRace from '../model/direct-race';
+import SignedUp from '../model/signed-up';
+import SynchronousDinghy from './synchronous-model/synchronous-dinghy';
+import SynchronousEntry from './synchronous-model/synchronous-entry';
+
+vi.mock('../model/sylph-model');
+vi.mock('../model/clock');
+
+it('renders', () => {
+    const model = new SylphModel(httpRootURL, wsRootURL);
+    const entryChrisMarshallScorpionA1234 = new SynchronousEntry(
+        new Entry(entryChrisMarshall1234ScorpionAHAL, {version: '"0"', model}), 
+        new SynchronousDinghy(new Dinghy(dinghy1234HAL, {version: '"0"'}, model), new DinghyClass(dinghyClassScorpionHAL, {version: '"0"'}, model)),
+        new Competitor(competitorChrisMarshallHAL, {version: '"0"'}, model),
+        new DirectRace(raceScorpionAHAL, {version: '"0"'}, model),
+        new Collection([], {size: 20, totalElements: 0, totalPages: 0,number: 0}),
+        new SignedUp(signedUpChrisMarshallDinghy1234ScorpionAHAL, {version: '"0"'}, model)
+    );
+    render(<EditRaceEntryView entry={entryChrisMarshallScorpionA1234} />);
+    const SMScorp1234entry = screen.getByRole('status', {name: (content, node) => node.textContent === '1234'});
+    expect(SMScorp1234entry).toBeInTheDocument();
+});
